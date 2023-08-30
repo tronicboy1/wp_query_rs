3820193d7c0b173201d58e7bab73961d1585a6ba
Austin John Mayer (井上 オースティン) <mayeraus41@gmail.com>
2023-08-30 18:00:28 +0900

    use onceLock not oncecell

--- src/sql/pool.rs
+++ src/sql/pool.rs
@@ -1,9 +1,9 @@
 use mysql::Pool;
-use once_cell::sync::OnceCell;
+use std::sync::OnceLock;
 
 use super::{build_opts_from_env, EnvVars};
 
-static POOL_INSTANCE: OnceCell<Pool> = OnceCell::new();
+static POOL_INSTANCE: OnceLock<Pool> = OnceLock::new();
 
 pub fn get_pool() -> &'static Pool {
     POOL_INSTANCE.get_or_init(|| {
